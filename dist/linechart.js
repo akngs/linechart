a0fa8c6e24becd90b3aca7a9f0208c0e8195167c
akngs <jania902@gmail.com>
2016-04-01 09:01:28 +0900

    .

--- dist/linechart.js
+++ dist/linechart.js
@@ -80,120 +80,52 @@ return /******/ (function(modules) { // webpackBootstrap
 	    _update();
 	  };
 	
-	  // Properties
-	  var data = [];
-	  instance.data = function (value) {
-	    return arguments.length ? (data = value, instance) : data;
+	  var props = {
+	    // Data
+	    data: [],
+	    xFocus: null,
+	    yFocus: null,
+	    pMarkers: [],
+	    xMarkers: [],
+	    yMarkers: [],
+	    xAccessor: function xAccessor(d, i) {
+	      return i;
+	    },
+	    yAccessor: function yAccessor(d, i) {
+	      return d;
+	    },
+	    xScaleType: 'linear',
+	    yScaleType: 'linear',
+	    interpolate: 'linear',
+	
+	    // Style
+	    xAxisTickFormat: null,
+	    yAxisTickFormat: null,
+	    xAxisTickInside: false,
+	    yAxisTickInside: false,
+	    width: 300,
+	    height: 150,
+	    marginL: 24,
+	    marginR: 18,
+	    marginT: 12,
+	    marginB: 18,
+	    paddingL: 8,
+	    paddingB: 8,
+	    showDataLine: true,
+	    showDataPoint: true
 	  };
 	
-	  var xFocus = null;
-	  instance.xFocus = function (value) {
-	    return arguments.length ? (xFocus = value, instance) : xFocus;
+	  // Generate property accessors
+	
+	  var _loop = function _loop(key) {
+	    instance[key] = function (value) {
+	      return arguments.length ? (props[key] = value, instance) : props[key];
+	    };
 	  };
 	
-	  var yFocus = null;
-	  instance.yFocus = function (value) {
-	    return arguments.length ? (yFocus = value, instance) : yFocus;
-	  };
-	
-	  var pMarkers = [];
-	  instance.pMarkers = function (value) {
-	    return arguments.length ? (pMarkers = value, instance) : pMarkers;
-	  };
-	
-	  var xMarkers = [];
-	  instance.xMarkers = function (value) {
-	    return arguments.length ? (xMarkers = value, instance) : xMarkers;
-	  };
-	
-	  var yMarkers = [];
-	  instance.yMarkers = function (value) {
-	    return arguments.length ? (yMarkers = value, instance) : yMarkers;
-	  };
-	
-	  var xAccessor = function xAccessor(d, i) {
-	    return i;
-	  };
-	  instance.xAccessor = function (value) {
-	    return arguments.length ? (xAccessor = value, instance) : xAccessor;
-	  };
-	
-	  var yAccessor = function yAccessor(d, i) {
-	    return d;
-	  };
-	  instance.yAccessor = function (value) {
-	    return arguments.length ? (yAccessor = value, instance) : yAccessor;
-	  };
-	
-	  var xScaleType = 'linear';
-	  instance.xScaleType = function (value) {
-	    return arguments.length ? (xScaleType = value, instance) : xScaleType;
-	  };
-	
-	  var yScaleType = 'linear';
-	  instance.yScaleType = function (value) {
-	    return arguments.length ? (yScaleType = value, instance) : yScaleType;
-	  };
-	
-	  var yAxisTickFormat = null;
-	  instance.yAxisTickFormat = function (value) {
-	    return arguments.length ? (yAxisTickFormat = value, instance) : yAxisTickFormat;
-	  };
-	
-	  var xAxisTickFormat = null;
-	  instance.xAxisTickFormat = function (value) {
-	    return arguments.length ? (xAxisTickFormat = value, instance) : xAxisTickFormat;
-	  };
-	
-	  var width = 300;
-	  instance.width = function (value) {
-	    return arguments.length ? (width = value, instance) : width;
-	  };
-	
-	  var height = 150;
-	  instance.height = function (value) {
-	    return arguments.length ? (height = value, instance) : height;
-	  };
-	
-	  var marginL = 24;
-	  instance.marginL = function (value) {
-	    return arguments.length ? (marginL = value, instance) : marginL;
-	  };
-	
-	  var marginR = 18;
-	  instance.marginR = function (value) {
-	    return arguments.length ? (marginR = value, instance) : marginR;
-	  };
-	
-	  var marginT = 12;
-	  instance.marginT = function (value) {
-	    return arguments.length ? (marginT = value, instance) : marginT;
-	  };
-	
-	  var marginB = 18;
-	  instance.marginB = function (value) {
-	    return arguments.length ? (marginB = value, instance) : marginB;
-	  };
-	
-	  var paddingL = 8;
-	  instance.paddingL = function (value) {
-	    return arguments.length ? (paddingL = value, instance) : paddingL;
-	  };
-	
-	  var paddingB = 8;
-	  instance.paddingB = function (value) {
-	    return arguments.length ? (paddingB = value, instance) : paddingB;
-	  };
-	
-	  var showDataLine = true;
-	  instance.showDataLine = function (value) {
-	    return arguments.length ? (showDataLine = value, instance) : showDataLine;
-	  };
-	
-	  var showDataPoint = true;
-	  instance.showDataPoint = function (value) {
-	    return arguments.length ? (showDataPoint = value, instance) : showDataPoint;
-	  };
+	  for (var key in props) {
+	    _loop(key);
+	  }
 	
 	  // Event handlers
 	  var mouseoverHandler = function mouseoverHandler(x, y) {};
@@ -271,35 +203,35 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	  var _update = function _update() {
 	    // Update scales
-	    _xScale = new SCALE_TYPES[xScaleType]().domain(_getExtent(data, xAccessor, xMarkers, pMarkers)).rangeRound([0, width - marginL - marginR - paddingL]);
-	    _yScale = new SCALE_TYPES[yScaleType]().domain(_getExtent(data, yAccessor, yMarkers, pMarkers)).rangeRound([height - marginT - marginB - paddingB, 0]);
+	    _xScale = new SCALE_TYPES[props.xScaleType]().domain(_getExtent(props.data, props.xAccessor, props.xMarkers, props.pMarkers)).rangeRound([0, props.width - props.marginL - props.marginR - props.paddingL]);
+	    _yScale = new SCALE_TYPES[props.yScaleType]().domain(_getExtent(props.data, props.yAccessor, props.yMarkers, props.pMarkers)).rangeRound([props.height - props.marginT - props.marginB - props.paddingB, 0]);
 	
 	    var innerWidth = _xScale.range()[1];
 	    var innerHeight = _yScale.range()[0];
 	    var color = _d3.default.scale.category10();
 	    var xScaledAccessor = function xScaledAccessor(d, i) {
-	      return _xScale(xAccessor(d, i));
+	      return _xScale(props.xAccessor(d, i));
 	    };
 	    var yScaledAccessor = function yScaledAccessor(d, i) {
-	      return _yScale(yAccessor(d, i));
+	      return _yScale(props.yAccessor(d, i));
 	    };
 	
 	    // Resize container element
-	    _containerEl.setAttribute('width', width);
-	    _containerEl.setAttribute('height', height);
-	    _rootSel.attr('transform', 'translate(' + (marginL + paddingL) + ', ' + marginT + ')');
+	    _containerEl.setAttribute('width', props.width);
+	    _containerEl.setAttribute('height', props.height);
+	    _rootSel.attr('transform', 'translate(' + (props.marginL + props.paddingL) + ', ' + props.marginT + ')');
 	
 	    // Render axes
-	    var xAxis = _d3.default.svg.axis().scale(_xScale).tickFormat(xAxisTickFormat).tickSize(2).ticks(5).orient('bottom');
-	    var yAxis = _d3.default.svg.axis().scale(_yScale).tickFormat(yAxisTickFormat).tickSize(2).ticks(5).orient('left');
+	    var xAxis = _d3.default.svg.axis().scale(_xScale).tickFormat(props.xAxisTickFormat).tickSize(props.xAxisTickInside ? -innerHeight - props.paddingB : 2).ticks(5).orient('bottom');
+	    var yAxis = _d3.default.svg.axis().scale(_yScale).tickFormat(props.yAxisTickFormat).tickSize(props.yAxisTickInside ? -innerWidth - props.paddingL : 2).ticks(5).orient('left');
 	
-	    _xAxisSel.attr('transform', 'translate(0, ' + (innerHeight + paddingB) + ')').call(xAxis);
-	    _yAxisSel.attr('transform', 'translate(' + -paddingL + ', 0)').call(yAxis);
+	    _xAxisSel.attr('transform', 'translate(0, ' + (innerHeight + props.paddingB) + ')').call(xAxis);
+	    _yAxisSel.attr('transform', 'translate(' + -props.paddingL + ', 0)').call(yAxis);
 	
 	    // Render data lines
-	    var line = _d3.default.svg.line().x(xScaledAccessor).y(yScaledAccessor);
+	    var line = _d3.default.svg.line().x(xScaledAccessor).y(yScaledAccessor).interpolate(props.interpolate);
 	
-	    var lineSel = _plotSel.selectAll('.line').data(showDataLine ? data : [], function (d) {
+	    var lineSel = _plotSel.selectAll('.line').data(props.showDataLine ? props.data : [], function (d) {
 	      return d.key;
 	    });
 	
@@ -314,7 +246,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    });
 	
 	    // Render data points
-	    var pointsSel = _plotSel.selectAll('.points').data(showDataPoint ? data : [], function (d) {
+	    var pointsSel = _plotSel.selectAll('.points').data(props.showDataPoint ? props.data : [], function (d) {
 	      return d.key;
 	    });
 	
@@ -335,7 +267,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }).attr('stroke-width', 1).attr('fill', 'none');
 	
 	    // Render point markers
-	    var pMarkerSel = _pMarkersSel.selectAll('.marker').data(pMarkers);
+	    var pMarkerSel = _pMarkersSel.selectAll('.marker').data(props.pMarkers);
 	
 	    pMarkerSel.enter().append('g').attr('class', 'marker').each(function () {
 	      var sel = _d3.default.select(this);
@@ -350,31 +282,31 @@ return /******/ (function(modules) { // webpackBootstrap
 	    });
 	
 	    // Render x focus
-	    _xFocusSel.attr('stroke-width', xFocus === null ? 0 : 0.5).attr('y2', innerHeight + paddingB).attr('x1', _xScale(xFocus)).attr('x2', _xScale(xFocus));
+	    _xFocusSel.attr('stroke-width', props.xFocus === null ? 0 : 0.5).attr('y2', innerHeight + props.paddingB).attr('x1', _xScale(props.xFocus)).attr('x2', _xScale(props.xFocus));
 	
 	    // Render y focus
-	    _yFocusSel.attr('transform', 'translate(' + -paddingL + ', 0)');
-	    _yFocusSel.attr('stroke-width', yFocus === null ? 0 : 0.5).attr('x2', innerWidth + paddingL).attr('y1', _yScale(yFocus)).attr('y2', _yScale(yFocus));
+	    _yFocusSel.attr('transform', 'translate(' + -props.paddingL + ', 0)');
+	    _yFocusSel.attr('stroke-width', props.yFocus === null ? 0 : 0.5).attr('x2', innerWidth + props.paddingL).attr('y1', _yScale(props.yFocus)).attr('y2', _yScale(props.yFocus));
 	
 	    // Render x markers
-	    _xMarkersSel.attr('transform', 'translate(' + -paddingL + ', 0)');
-	    var xMarkerSel = _xMarkersSel.selectAll('.marker').data(xMarkers);
+	    _xMarkersSel.attr('transform', 'translate(' + -props.paddingL + ', 0)');
+	    var xMarkerSel = _xMarkersSel.selectAll('.marker').data(props.xMarkers);
 	
 	    xMarkerSel.enter().append('line').attr('class', 'marker').attr('stroke-width', 0.5);
 	
 	    xMarkerSel.exit().remove();
 	
-	    xMarkerSel.attr('x1', _xScale).attr('x2', _xScale).attr('y2', innerHeight + paddingB);
+	    xMarkerSel.attr('x1', _xScale).attr('x2', _xScale).attr('y2', innerHeight + props.paddingB);
 	
 	    // Render y markers
-	    _yMarkersSel.attr('transform', 'translate(' + -paddingL + ', 0)');
-	    var yMarkerSel = _yMarkersSel.selectAll('.marker').data(yMarkers);
+	    _yMarkersSel.attr('transform', 'translate(' + -props.paddingL + ', 0)');
+	    var yMarkerSel = _yMarkersSel.selectAll('.marker').data(props.yMarkers);
 	
 	    yMarkerSel.enter().append('line').attr('class', 'marker').attr('stroke-width', 0.5);
 	
 	    yMarkerSel.exit().remove();
 	
-	    yMarkerSel.attr('y1', _yScale).attr('y2', _yScale).attr('x2', innerWidth + paddingL);
+	    yMarkerSel.attr('y1', _yScale).attr('y2', _yScale).attr('x2', innerWidth + props.paddingL);
 	
 	    // Resize overlay
 	    _overlaySel.attr('width', innerWidth).attr('height', innerHeight);
@@ -453,7 +385,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	
 	  var _mouseToDomain = function _mouseToDomain(x, y) {
-	    return [_xScale.invert(x - marginL - paddingL), _yScale.invert(y - marginT)];
+	    return [_xScale.invert(x - props.marginL - props.paddingL), _yScale.invert(y - props.marginT)];
 	  };
 	
 	  instance.update = _update;
